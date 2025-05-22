15a6cea0f24eab5c870909b3356b6333192fb6f1
felipithstdr <felipiths58@gmail.com>
2025-05-22 01:20:45 -0300

    fix: correcao de migration

--- prisma/migrations/20250521193724_up_dc/migration.sql
+++ prisma/migrations/20250521193724_up_dc/migration.sql
@@ -4,6 +4,11 @@
   - A unique constraint covering the columns `[ecpf]` on the table `DigitalCertificate` will be added. If there are existing duplicate values, this will fail.
 
 */
+
+-- AlterTable: renomeando cnpj para ecnpj
+ALTER TABLE "DigitalCertificate"
+RENAME COLUMN "cnpj" TO "ecnpj";
+
 -- AlterTable
 ALTER TABLE "DigitalCertificate" ADD COLUMN     "ecpf" TEXT,
 ADD COLUMN     "issueCode" TEXT,
@@ -12,3 +17,4 @@ ADD COLUMN     "protocol" TEXT;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "DigitalCertificate_ecpf_key" ON "DigitalCertificate"("ecpf");
+CREATE UNIQUE INDEX "DigitalCertificate_ecnpj_key" ON "DigitalCertificate"("ecnpj");
